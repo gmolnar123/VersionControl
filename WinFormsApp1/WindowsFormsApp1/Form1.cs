d3c97fdc64be9cf71a6dcc74b7736188bc4b0428
gmolnar285 <40260228+gmolnar285@users.noreply.github.com>
2022-11-11 10:16:40 +0100

    3

--- WinFormsApp1/WindowsFormsApp1/Form1.cs
+++ WinFormsApp1/WindowsFormsApp1/Form1.cs
@@ -14,6 +14,8 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         List<Country> countries = new List<Country>();
+        List<Ramen> ramens = new List<Ramen>();
+
         public Form1()
         {
             InitializeComponent();
@@ -26,22 +28,39 @@ namespace WindowsFormsApp1
             { 
               sr.ReadLine();
               while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split(';');
+                    string orszag = sor[2];
+                    Country or = AddCountry(orszag);
+                    Ramen r = new Ramen
+                    {
+                        ID = ramens.Count,
+                        CountryFK = or.ID,
+                        Country = or,
+                        Stars = Convert.ToDouble(sor[3]),
+                        Brand = sor[0],
+                        Name = sor[1]
+                    };
+                    ramens.Add(r);
+
+
+                }
+            }
+
+            Country AddCountry(string orszag)
             {
-                string[] sor = sr.ReadLine().Split(';');
-                string orszag = sor[2];
                 var ered = (from c in countries where c.Name.Equals(orszag) select c).FirstOrDefault();
                 if (ered == null)
+                {
+                    ered = new Country()
                     {
-                        ered = new Country()
-                        {
-                            ID = countries.Count + 1,
-                            Name = orszag
-                        };
-                        countries.Add(ered);
-                    }
-
+                        ID = countries.Count + 1,
+                        Name = orszag
+                    };
+                    countries.Add(ered);
+                }
+                return ered;
             }
-        }
         }
     }
 }
