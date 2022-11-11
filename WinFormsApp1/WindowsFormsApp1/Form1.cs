1d1bde87a0d4af9fb6b73250d4b1e6966a5ed8ed
gmolnar285 <40260228+gmolnar285@users.noreply.github.com>
2022-11-11 09:57:20 +0100

    2.

--- WinFormsApp1/WindowsFormsApp1/Form1.cs
+++ WinFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,35 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        List<Country> countries = new List<Country>();
         public Form1()
         {
             InitializeComponent();
+            LoadData("Ramen.csv");
+        }
+
+        public void LoadData(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName)) 
+            { 
+              sr.ReadLine();
+              while (!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(';');
+                string orszag = sor[2];
+                var ered = (from c in countries where c.Name.Equals(orszag) select c).FirstOrDefault();
+                if (ered == null)
+                    {
+                        ered = new Country()
+                        {
+                            ID = countries.Count + 1,
+                            Name = orszag
+                        };
+                        countries.Add(ered);
+                    }
+
+            }
+        }
         }
     }
 }
