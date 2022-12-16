a6cbc908f73df87bb2b3466e43105e21e0c12cf5
gmolnar285 <40260228+gmolnar285@users.noreply.github.com>
2022-12-16 19:38:28 +0100

    fel5

---
+++ WindowsFormsApp1/WindowsFormsApp1/Entities/PortfolioItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Entities
+{
+    internal class PortfolioItem
+    {
+        public string Index { get; set; }
+        public decimal Vollume { get; set; }
+    }
+}
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Entities;
 
 namespace WindowsFormsApp1
 {
@@ -15,12 +16,23 @@ namespace WindowsFormsApp1
 
         List<Tick> ticks;
         PortfolioEntities pe = new PortfolioEntities();
+        List<PortfolioItem> pi = new List<PortfolioItem>();
+
 
         public Form1()
         {
             InitializeComponent();
             ticks = pe.Tick.ToList();
             dataGridView1.DataSource = ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            pi.Add(new PortfolioItem() { Index = "OTP", Vollume = 10 });
+            pi.Add(new PortfolioItem() { Index = "ZWACK", Vollume = 10 });
+            pi.Add(new PortfolioItem() { Index = "ELMU", Vollume = 10 });
+            dataGridView2.DataSource = pi;
         }
     }
 }
